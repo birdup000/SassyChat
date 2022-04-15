fdcd6fe59f6da6ecf33c5f459115335d14e7a7b7
braden644 <34012548+greengeckowizard@users.noreply.github.com>
2022-04-14 20:50:28 -0500

    somewhere at least

--- main.js
+++ main.js
@@ -4,11 +4,18 @@ const app = express();
 const path = require('path')
 const {v4: uuidV4} = require('uuid')
 const bodyParser = require('body-parser'); // middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-
+app.use(bodyParser.urlencoded({ extended: true }));
+var session = require('express-session');
+var multer = require('multer');
+var upload = multer(); 
+var cookieParser = require('cookie-parser');
+app.use(upload.array());
+app.use(cookieParser());
 var port = 8080;
+app.use(session({secret:'Keep it secret'
+,name:'uniqueSessionID'
+,saveUninitialized:false}))
+const users = require("./users.json");
 
 
 
@@ -25,7 +32,10 @@ const server = http.createServer(app);
 
 const io = socketio(server);
 
-
+var Users =[{
+  username: 'gecko',
+  password: 'Hotwheel1'
+}] 
 
 app.use(express.json())
 
@@ -82,15 +92,33 @@ app.get('/chatlogged', (req, res) =>{
   res.render("chatlogged")
  })
 
+ 
+
+
 app.post("/login", (req, res) => {
   let form = req.body;
   let username = req.body["username"];
   let password = req.body["password"];
-  //chack user login
- res.send(` Username:${username} Password:${password}`)
+ //more cool cookiestuff 
  
+ })
+ 
+
+  //chack user login
+ //res.send(` Username:${username} Password:${password}`)
+ 
+
   console.log(`\x1b[42mNEW LOGIN REQUEST\x1b[0m: (${username} | ${password})`)
 })
+
+app.get('/logout', function(req, res){
+  req.session.destroy(function(){
+     console.log("user logged out.")
+  });
+  res.redirect('/login');
+});
+
+
 
 
 app.post("/signup", (req, res) => {
@@ -100,8 +128,28 @@ app.post("/signup", (req, res) => {
   let email = req.body["email"];
   //chack user signup
   console.log(`\x1b[42mNEW SIGN UP REQUEST\x1b[0m: (${username} | ${password} | ${email})`)
-})
+///add signup cool cookie stuff
+if(!req.body.username || !req.body.password){
+  res.status("400");
+  res.send("Invalid details!");
+} else {
+  Users.filter(function(user){
+     if(user.username === req.body.username){
+        res.render('signup', {
+           message: "User Already Exists! Login or choose another user id"});
+     }
+  });
+  var newUser = {username: req.body.username, password: req.body.password};
+  Users.push(newUser);
+  req.session.user = newUser;
+  res.redirect('/protected_page');
+}
+});
 
+
+app.get('/protected_page', checkSignIn, function(req, res){
+  res.render('protected_page', {username: req.session.user.username})
+});
 
 ////app.use(function (req,res,next){
 ////	res.status(404).render("404");
@@ -189,6 +237,10 @@ io.on('connection', socket => {
       })
   })
 })
+
+
+///add this is the login and signup features still experimental!@
+
 
 
 
