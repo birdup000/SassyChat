536441acda1836bc7c43aa89d643cb9ca3b77263
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-02-26 18:51:59 -0600

    mightwork

--- views/script2.js
+++ views/script2.js
@@ -7,19 +7,25 @@ const audioGrid = document.getElementById('audio-grid') // Find the Audio-Grid e
  
 const myPeer = new Peer() // Creating a peer element which represents the current user
 const myAudio = document.createElement('audio') // Create a new audio tag to show our audio
+myAudio.id = 'my-audio'; // Set the ID of the audio element
 
 // Access the user's just audio
 navigator.mediaDevices.getUserMedia({
     audio: true,
     video: false
 }).then(stream => {
+    console.log('Got Audio Stram', stream)
     myAudio.srcObject = stream // Set our audio to use our local audio stream
     myAudio.muted = true // Mute our own audio so we don't hear ourselves
     myAudio.play() // Start playing our audio
+    .catch(error => {
+        console.error('Error getting audio stream:', error)
+      })
 
     myPeer.on('call', call => { // When we join someone's room we will receive a call from them
         call.answer(stream) // Stream them our audio
         const audio = document.createElement('audio') // Create an audio tag for them
+        audio.id = call.peer; // Set the ID of the audio element to the peer ID
         call.on('stream', userAudioStream => { // When we recieve their stream
             addAudioStream(audio, userAudioStream) // Display their audio to the screen
         })
@@ -27,6 +33,7 @@ navigator.mediaDevices.getUserMedia({
 
     socket.on('user-connected', userId => { // If a new user connect
         connectToNewUser(userId, stream) 
+        console.log('connectToNewUser called with userId:', userId);
     })
 })
 
@@ -38,6 +45,7 @@ function connectToNewUser(userId, stream) { // This runs when someone joins our 
     const call = myPeer.call(userId, stream) // Call the user who just joined
     // Add their audio
     const audio = document.createElement('audio') 
+    audio.id = call.peer; // Set the ID of the audio element to the peer ID
     call.on('stream', userAudioStream => {
         addAudioStream(audio, userAudioStream)
     })
@@ -48,11 +56,10 @@ function connectToNewUser(userId, stream) { // This runs when someone joins our 
 }
 
 function addAudioStream(audio, stream) {
-    audio.srcObject = new MediaStream([stream.getAudioTracks()[0]]);
+    audio.srcObject = stream;
     audio.addEventListener('loadedmetadata', () => {
-      audio.play();
+        audio.play();
     });
     audioGrid.appendChild(audio);
-  }
-  
-  
+}
+
